b8cfd8002b2be383fc58a8b3b3c752f35baf52dd
David Lenihan <davidlenihan@outlook.com>
2022-02-21 20:44:16 -0800

    WIP: Adding first app

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # tron - Qt/OpenSceneGraph based libraries and apps
 
+## Prerequisites
+
+### Windows
+
+1. Install [PowerShell 7+ from Microsoft Store](https://www.microsoft.com/en-us/p/powershell/9mz1snwt0n5d)
+2. Install Visual Studio 2022 Community
+   1. Download and open [vs_Community.exe](https://aka.ms/vs/17/release/vs_community.exe)
+   2.  Select *"Desktop development with C++"* Workload
+   3.  Click *"Install"* button
+
+### Ubuntu 20.04
+
+1. [Installing PowerShell on Ubuntu](https://docs.microsoft.com/en-us/powershell/scripting/install/install-ubuntu)
+
+### Mac
+
+1. [Installing PowerShell on macOS](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-macos)
+
 ## Setup
+
+1. Open PowerShell 7+ (pwsh) prompt
+2. Update environment for dev tools
+   * *WINDOWS* `& "$env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1"`
+3. `cd <TRON_REPO>`
+
+3. `.\setup.ps1`
 
 ## Build
 
@@ -17,6 +42,8 @@ out/meta
 * Fast - Want to be able to develop fast, debug fast, iterate fast, run fast
 * Easy - Things should "just work." 
 * Crossplatform - Windows, Linux, Mac
+* Multiple copies of repo 
+* Run locally* 
 
 ## Hierarchy
 
@@ -46,12 +73,10 @@ out/meta
     * x64-linux_release
   * assets
   * scripts
-    * setup
-    * setup.bat
-    * setup.ps1
   * third_party
     * vcpkg
   * ide
     * vscode
   * README.md
+  * setup.ps1
 
---
+++ setup.ps1
@@ -0,0 +1,93 @@
+# Install prerequisites, builds third party libraries
+
+# Prerequisites
+Write-Host "Prerequisites..." -ForegroundColor Cyan
+if ($IsWindows)
+{
+    $cmd = " winget install --id Kitware.Cmake"
+    Write-Host $cmd -ForegroundColor Cyan
+    Invoke-Expression $cmd
+}
+elseif ($IsLinux) {
+    # Get latest versions of software
+    sudo apt update
+        
+    # for WSL build from Visual Studio
+    sudo apt install -y g++
+    sudo apt install -y gdb 
+    sudo apt install -y make 
+    sudo apt install -y ninja-build 
+    sudo apt install -y rsync 
+    sudo apt install -y zip
+    
+    # needed by osg
+    sudo apt install -y gperf
+
+    # Needed by qt5
+    sudo apt install -y bison
+    sudo apt install -y python
+
+    # Needed for qt5 https://doc.qt.io/qt-5/linux-requirements.html
+    sudo apt install -y libfontconfig1-dev
+    sudo apt install -y libfreetype6-dev
+    sudo apt install -y libx11-dev
+    sudo apt install -y libx11-xcb-dev
+    sudo apt install -y libxext-dev
+    sudo apt install -y libxfixes-dev
+    sudo apt install -y libxi-dev
+    sudo apt install -y libxrender-dev
+    sudo apt install -y libxcb1-dev
+    sudo apt install -y libxcb-glx0-dev
+    sudo apt install -y libxcb-keysyms1-dev
+    sudo apt install -y libxcb-image0-dev
+    sudo apt install -y libxcb-shm0-dev
+    sudo apt install -y libxcb-icccm4-dev
+    sudo apt install -y libxcb-sync0-dev
+    sudo apt install -y libxcb-xfixes0-dev
+    sudo apt install -y libxcb-shape0-dev
+    sudo apt install -y libxcb-randr0-dev
+    sudo apt install -y libxcb-render-util0-dev
+    sudo apt install -y libxcd-xinerama-dev
+    sudo apt install -y libxkbcommon-dev
+    sudo apt install -y libxkbcommon-x11-dev
+
+    # Needed for qt5 https://github.com/microsoft/vcpkg/blob/master/scripts/azure-pipelines/linux/provision-image.sh
+    sudo apt install -y libxcb-util0-dev
+    sudo apt install -y libxcb-xinerama0-dev
+    sudo apt install -y libxcb-xkb-dev
+    sudo apt install -y libxcb-xinput-dev
+}
+
+# Build third party libraries
+Write-Host "Building third party libraries..." -ForegroundColor Cyan
+$ROOT_DIR = $PSScriptRoot
+$THIRD_PARTY_DIR = "$ROOT_DIR\third_party"
+mkdir $THIRD_PARTY_DIR -Force | Out-Null
+$VCPKG_DIR = "$THIRD_PARTY_DIR\vcpkg"
+$TAG = "2022.02.02"
+Write-Host "Using vcpkg tag $TAG" -ForegroundColor Cyan
+git clone --branch $TAG https://github.com/Microsoft/vcpkg.git $VCPKG_DIR 
+if ($IsWindows) {
+    $triplet = "x64-windows"   # dynamic library, dynamic CRT
+    $cmd = "$VCPKG_DIR/bootstrap-vcpkg.bat -disableMetrics"
+    Write-Host $cmd -ForegroundColor Cyan
+    Invoke-Expression $cmd
+}    
+#     # To avoid path length issues building qt5 (qtdeclarative), make root as short as possible
+#     # From https://github.com/microsoft/vcpkg/issues/20604#issuecomment-939091795
+#     subst k: $ROOT_DIR\third_party\vcpkg
+#     Set-Location k:
+# } elseif ($IsLinux) {
+    #     $triplet = "x64-linux"
+    #     ./bootstrap-vcpkg.sh -disableMetrics
+    # }
+    
+$packages = 
+    "fontconfig",                       # needed by osg for reading fonts. Windows: 20 min
+    "osg[tools,plugins,examples]",      #                                  Windows: 1.4 hours
+    "qt5"                               #                                  Windows: 1.2 hours
+foreach ($pkg in $packages) {
+    $cmd = "$VCPKG_DIR/vcpkg --triplet=$triplet --recurse install $pkg"
+    Write-Host $cmd -ForegroundColor Cyan
+    Invoke-Expression "$VCPKG_DIR/vcpkg --triplet=$triplet --recurse install $pkg"
+}
