2a145ac9de621a80cc5141b7ce8e74ac7797c581
David Lenihan <davidlenihan@outlook.com>
2022-03-04 21:54:43 -0800

    Adding hello_cmake and hello_osg

---
+++ src/sandbox/hello_cmake/hello_cmake.cpp
@@ -0,0 +1,12 @@
+﻿// hello_cmake.cpp : Defines the entry point for the application.
+//
+
+#include "hello_cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+ 	return 0;
+}
