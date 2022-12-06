3488502b97cde5b4bea2aba08851e6980ee0b960
Stan Podlesny <spodlesnyy@argo.ai>
2022-12-06 14:00:54 -0500

    added hello_local_repo example

---
+++ src/hello/hello_local_repo/dll/helper.cc
@@ -0,0 +1,7 @@
+#include "helper.hh"
+#include <iostream>
+
+void hello_helper()
+{
+  std::cout << "I am a helper lib" << std::endl;
+}
---
+++ src/hello/hello_local_repo/exe/main.cc
@@ -0,0 +1,7 @@
+#include "helper.hh"
+
+
+int main(int argc, char** argv) {
+  hello_helper();
+  return 0;
+}
