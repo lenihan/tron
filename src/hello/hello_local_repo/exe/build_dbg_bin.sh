9e47e0b104e3cfcbb73f0d581978e5e3f157f97f
Stan Podlesny <spodlesnyy@argo.ai>
2022-12-09 14:03:05 -0500

    fixed typos

--- src/hello/hello_local_repo/exe/build_dbg_bin.sh
+++ src/hello/hello_local_repo/exe/build_dbg_bin.sh
@@ -1,6 +1,9 @@
 @!/bin/bash
 
-bash -c 'cd ../dll; bazel build helper'
-bash -c 'cd exe; bazel build -c dbg main'
+bash -c 'cd ../dll; bazel build -c opt helper'
+bash -c 'bazel build -c dbg main'
 
-# In other to run with grb, keep staying in exe directory
+# In other to run with gdb, keep staying in exe directory
+
+# ldd bazel-bin/main
+# gdb bazel-bin/main
